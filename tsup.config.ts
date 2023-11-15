58f5a492acbc098700da47c9edba477b2c050109
eqpoqpe <eqpoqpe@gmail.com>
2023-11-15 21:02:35 +0800

    fix css prefixer

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from "tsup";
 import { vanillaExtractPlugin } from "@vanilla-extract/esbuild-plugin";
+import postcss from "postcss";
+import autoprefixer from "autoprefixer";
+
+async function processCss(css: string) {
+  const result = await postcss([autoprefixer]).process(css, {
+    from: undefined,
+  });
+
+  return result.css;
+}
 
 const tsupConfig = defineConfig({
   esbuildPlugins: [
     vanillaExtractPlugin({
       identifiers: ({ hash }) => `zen_ui_${hash}`,
+      processCss: processCss,
     }) as unknown as any,
   ],
   entry: ["./src/index.ts", "./src/kit/index.ts"],
