e634b11914bcccd45eb6421066d780d4df0c8c16
Ryan Martin <eqpoqpe@gmail.com>
2024-03-07 20:12:46 +0800

    fix import css file

--- packages/dialog/src/style.css.ts
+++ packages/dialog/src/style.css.ts
@@ -1,60 +1,47 @@
 import { style } from "@vanilla-extract/css";
 
-const dialogBaseStyle = style({
+export const dialogBaseStyle = style({
   position: "relative",
   zIndex: 3000,
 });
 
-const backdropBaseStyle = style({
+export const backdropBaseStyle = style({
   position: "fixed",
   inset: "0",
 });
 
-const backdropBackgroundStyle = style({
+export const backdropBackgroundStyle = style({
   backgroundColor: "#00000066",
 });
 
-const dialogPanelBaseStyle = style({
+export const dialogPanelBaseStyle = style({
   position: "fixed",
   inset: "0",
   overflow: "hidden",
 });
 
-const backdropEnterStyle = style({
+export const backdropEnterStyle = style({
   transitionTimingFunction: "ease-out",
   transitionDuration: "200ms",
 });
 
-const backdropEnterFromStyle = style({
+export const backdropEnterFromStyle = style({
   opacity: 0,
 });
 
-const backdropEnterToStyle = style({
+export const backdropEnterToStyle = style({
   opacity: 1,
 });
 
-const backdropLeaveStyle = style({
+export const backdropLeaveStyle = style({
   transitionTimingFunction: "ease-in",
   transitionDuration: "100ms",
 });
 
-const backdropLeaveFromStyle = style({
+export const backdropLeaveFromStyle = style({
   opacity: 1,
 });
 
-const backdropLeaveToStyle = style({
+export const backdropLeaveToStyle = style({
   opacity: 0,
 });
-
-export {
-  dialogBaseStyle,
-  backdropBaseStyle,
-  backdropBackgroundStyle,
-  dialogPanelBaseStyle,
-  backdropEnterStyle,
-  backdropEnterFromStyle,
-  backdropEnterToStyle,
-  backdropLeaveStyle,
-  backdropLeaveFromStyle,
-  backdropLeaveToStyle,
-};
